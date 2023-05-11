79888131a6d7ad2e087bdd612a7a9efdb015ac55
Jake Klassen <jklassendev@gmail.com>
2023-05-11 16:41:36 -0400

    export control

--- packages/input/src/index.ts
+++ packages/input/src/index.ts
@@ -1,6 +1,7 @@
 export { Mouse } from './inputs/mouse.ts';
 export { Keyboard } from './inputs/keyboard.ts';
 export { Gamepad } from './inputs/gamepad.ts';
+export { Control } from './core/control.ts';
 
 export { and } from './core/and.ts';
 export { or } from './core/or.ts';
